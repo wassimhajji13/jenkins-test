7fe912bd27673f0e5173ef416ab9571c33a3eb93
wassimhajji13 <123875786+wassimhajji13@users.noreply.github.com>
2025-12-08 22:17:57 +0100

    Convert JavaScript bad code to Python
    Refactor bad code with Python syntax and remove unused functions.

--- bad-code.js
+++ bad-code.js
@@ -1,29 +1,12 @@
-// Code volontairement mauvais pour test SonarQube
+# Code très mauvais pour SonarQube
+password = "123456"   # secret exposé
 
-function add(a, b) {
+def add(a, b):
     return a + b
-}
 
-// Fonction inutile jamais utilisée
-function unusedFunction() {
-    let x = 10;
-    let y = 20;
-    return x * y;
-}
+def add(a, b):  # duplication
+    return a + b
 
-// Mot de passe en clair (grosse vulnérabilité)
-let password = "123456";
+if True == "True":     # bug logique
+   print("Bad code")
 
-// Comparaison toujours vraie (bug logique)
-if (true == "true") {
-    console.log("Bad logic here!");
-}
-
-// Code dupliqué
-function duplicate1() {
-    return add(1, 2);
-}
-
-function duplicate2() {
-    return add(1, 2);
-}
