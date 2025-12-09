e7125735d2d6074c89922b541f3d72654306ac01
wassimhajji13 <123875786+wassimhajji13@users.noreply.github.com>
2025-12-09 18:26:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("🚀  jwk fesfesssss !\n");
+    res.end("🚀  jwk mezmez !\n");
 });
 
 server.listen(80, () => {
