38c9066fb9af8d3f2b3dfa35f7cdf117f582d4b0
wassimhajji13 <123875786+wassimhajji13@users.noreply.github.com>
2025-12-09 17:57:58 +0100

    Update server response message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("🚀 Nouvelle version déployée automatiquement via CI/CD !\n");
+    res.end("🚀  jwk fesfes !\n");
 });
 
 server.listen(80, () => {
