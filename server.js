8a9c8e5389e608125a9696129966bbdba363b05d
wassimhajji13 <123875786+wassimhajji13@users.noreply.github.com>
2025-12-17 00:39:48 +0100

    Update server response message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("🚀  EKHER MARAAAAAAA !\n");
+    res.end("🚀  mara okhraaaaaaaaa!\n");
 });
 
 server.listen(80, () => {
